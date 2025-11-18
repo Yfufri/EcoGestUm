bcfe6ac69f48657e1e592f7bb48c722ad3fbd03f
allao <allaoui.mouad00@gmail.com>
2025-11-18 11:47:56 +0100

    Modif du style.css

--- views/VisiteurAccueil/Accueil.php
+++ views/VisiteurAccueil/Accueil.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <title>Votre titre</title>
     <link rel="stylesheet" href="../../assets/style.css">
 </head>
+
 <body>
     <div class="banner-recycler">
-    <div class="banner-overlay">
-        RECYCLER<br>
-        N’A JAMAIS<br>
-        ÉTÉ SI FACILE
+        <div class="banner-overlay">
+            RECYCLER<br>
+            N’A JAMAIS<br>
+            ÉTÉ SI FACILE
+        </div>
     </div>
-    </div>
-
+    <!-- Engagement Section -->
     <div class="engagement-section">
         <div class="engagement-Greenbox">
             <div class="engagement-Whitebox">
@@ -21,16 +23,42 @@
                     une université écoresponsable
                 </h2>
                 <p>
-                    À Le Mans Université, nous nous engageons à réduire notre impact environnemental en favorisant le recyclage, la réutilisation et la gestion durable des ressources.
+                    À Le Mans Université, nous nous engageons à réduire notre impact environnemental en favorisant le
+                    recyclage, la réutilisation et la gestion durable des ressources.
                 </p>
                 <p>
-                    Grâce à ÉcoGestUM, notre application dédiée, nous mettons en place des solutions innovantes pour optimiser le tri, encourager la réutilisation des objets et sensibiliser notre communauté universitaire aux enjeux écologiques.
+                    Grâce à ÉcoGestUM, notre application dédiée, nous mettons en place des solutions innovantes pour
+                    optimiser le tri, encourager la réutilisation des objets et sensibiliser notre communauté
+                    universitaire aux enjeux écologiques.
                 </p>
             </div>
-             <a href="#" class="policy-btn">Découvrir notre politique</a>
+            <a href="#" class="policy-btn">Découvrir notre politique</a>
+        </div>
+    </div>
+    <!-- Statistics Section -->
+    <?php
+    include('views/statistiquesEnv.html');
+    ?>
+    <!--Evenements Section -->
+    <div class="event-container">
+        <h1 class="event-title">Événements</h1>
+
+        <div class="photo-wrapper">
+            <img src="assets/Accueil/PhotoAccueil2.png" alt="Groupe d'étudiants ramassant des déchets" />
+
+            <div class="event-overlay">
+                <div class="event-desc">
+                    Des événements<br><strong>À NE PAS RATER!</strong>
+                </div>
+                <a href="#" class="policy-btn">S’inscrire à un événement</a>
+            </div>
+        </div>
+
+        <div class="event-caption">
+            Le Mans : des étudiants mobilisés pour ramasser des déchets
         </div>
     </div>
 
 </body>
-</html>
 
+</html>
