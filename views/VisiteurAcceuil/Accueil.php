52be9c5e9d11711f9df83ee93a353ba4ab047199
allao <allaoui.mouad00@gmail.com>
2025-11-18 08:42:35 +0100

    Ajout de VisiteurAcceuil

---
+++ views/VisiteurAcceuil/Accueil.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Votre titre</title>
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+<body>
+    <div class="banner-recycler">
+    <div class="banner-overlay">
+        RECYCLER<br>
+        N’A JAMAIS<br>
+        ÉTÉ SI FACILE
+    </div>
+    </div>
+
+    <div class="engagement-section">
+        <div class="engagement-box">
+            <h2>Notre Engagement pour<br>
+                une université écoresponsable
+            </h2>
+            <p>
+                À Le Mans Université, nous nous engageons à réduire notre impact environnemental en favorisant le recyclage, la réutilisation et la gestion durable des ressources.
+            </p>
+            <p>
+                Grâce à ÉcoGestUM, notre application dédiée, nous mettons en place des solutions innovantes pour optimiser le tri, encourager la réutilisation des objets et sensibiliser notre communauté universitaire aux enjeux écologiques.
+            </p>
+            <a href="#" class="policy-btn">Découvrir notre politique</a>
+        </div>
+    </div>
+
+</body>
+</html>
+
