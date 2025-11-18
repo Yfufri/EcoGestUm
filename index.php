9c3c6d876456466f629909339f3466422a0c9f1a
allao <allaoui.mouad00@gmail.com>
2025-11-18 11:42:46 +0100

    Modif style.css + Ajout bloc evenement dans Accueil

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 require_once "views/header.html";
-<<<<<<< HEAD
 require_once "views/VisiteurAccueil/Accueil.php";
-=======
-require_once "views/conseilEco.html";
->>>>>>> 09cf82bae8b42561b063d1eab7d7f3fb4877d505
 require_once "views/footer.html";
 ?>
