3450127396684facc9b38db70fcba4e41fb2a19e
lanox <kenanguerin0@gmail.com>
2025-11-19 09:05:41 +0100

    fix css

--- index.php
+++ index.php
@@ -2,7 +2,5 @@
 define('BASE_URL', '/EcoGestUM/');
 define('ASSETS_URL', BASE_URL . 'assets/');
 
-require_once "views/header.php";
 require_once  "views/connection.html";
-require_once  "views/footer.html";
 ?>  
