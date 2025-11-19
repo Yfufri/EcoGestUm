ea8952b7b888e3a8a296f814d87621df072873fc
Yfufri <theolannier50@gmail.com>
2025-11-19 14:06:45 +0100

    add fonts + banner.php

---
+++ views/banner.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="../assets/css/styleBanner.css">
+<link rel="stylesheet" href="../assets/css/styleAll.css">
+
+<div class="banner">
+  <img src="../assets/etudiant/image1.png" alt="Bannière" class="banner-img">
+  <div class="banner-text">
+    <h1>RECYCLER<br>N'A JAMAIS ÉTÉ SI FACILE</h1>
+  </div>
+</div>
