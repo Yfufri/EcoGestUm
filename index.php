6fbac1e1ded4ed18aa54adab0b3f2c334f854932
allao <allaoui.mouad00@gmail.com>
2025-11-19 11:51:25 +0100

    change style et index

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
+<<<<<<< HEAD
 require_once "views/header.html";
 require_once "views/VisiteurAccueil/Accueil.php";
 require_once "views/footer.html";
 ?>
+=======
+define('BASE_URL', '/EcoGestUM/');
+define('ASSETS_URL', BASE_URL . 'assets/');
+require_once "views/header.php";
+require_once "views/accueil_etudiant.php";
 
+require_once "views/footer.php";
+?>
+
+>>>>>>> f17799b6f903ae67fa8cc4db504ae4527af5653b
