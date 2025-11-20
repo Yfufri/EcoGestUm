074e46024bdb61d1cc1821a863e533342e02b142
lanox <kenanguerin0@gmail.com>
2025-11-20 13:33:27 +0100

    edit GererEvenement.php

--- models/gererEvenement.php
+++ models/gererEvenement.php
@@ -16,5 +16,16 @@ function getAllEventId($conn) {
     return $ids;
 }
 
+function getInfoEvent($conn,$id_event){
+    $sql = "SELECT evenement.Nom_evenement,evenement.Description,image_evenement.Url_image FROM evenement
+            INNER JOIN image_evenement 
+                ON image_evenement.Id_evenement=evenement.Id_evenement
+            WHERE evenement.Id_evenement=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id_event);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return $result->fetch_assoc();
+}
 
 ?>
