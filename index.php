08c7a6d2b0820006b0b27f6dfb20034f1ebfbe85
Yfufri <theolannier50@gmail.com>
2025-11-21 13:45:31 +0100

    fix

--- index.php
+++ index.php
@@ -24,21 +24,4 @@ require 'views/Footer.php';
 //}
 
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <title>ÉcoGestUM</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-
-<body>
-    <?php
-    require_once 'views/header.php';
-    require_once 'views/InscriptionEvent/inscription.php';
-    require_once 'views/footer.php';
-    ?>
-</body>
-
 </html>
