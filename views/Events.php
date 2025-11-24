af1f2a33db625a7f3b7d26e6ab8d7106f53c95c0
Yfufri <theolannier50@gmail.com>
2025-11-24 15:43:07 +0100

    Ne plus toucher a l'accueil visiteur svp

--- views/Events.php
+++ views/Events.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="assets/css/style.css">
-<link rel="stylesheet" href="assets/css/styleEvents.css">
+<link rel="stylesheet" href="assets/css/styleEvent.css">
 
 <div class="event-title">
 Evénements
