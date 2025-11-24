d2cc9a90c4759684b8b459493b49e94bcdf5115d
lanox <kenanguerin0@gmail.com>
2025-11-24 16:00:31 +0100

    routage etudiant

--- controllers/Home.php
+++ controllers/Home.php
@@ -1,7 +1,37 @@
 <?php
+
+include "models/gererUtilisateur.php";
+
+function displayDefaultHomePage($conn) {
         include "views/banner.php";
         include "views/DiscoverPolitique.php";
         include "controllers/Statistics.php";
+       
         include "views/Events.php";
-        include "views/visite.php";
+        include "views/Visits.php";
+
+        }
+
+function displayStudentHomePage($conn) {
+        include "views/banner.php";
+        include "views/welcome.php";
+        include "views/Recycle.php";
+        include "controllers/Statistics.php";
+        // point de collecte
+        echo "point de collecte";
+        }
+
+
+$sessionUser = $_SESSION['user'] ?? null;
+$userId = $sessionUser['Id_utilisateur'] ?? null;
+
+switch (getRole($conn, $userId)) {
+    case 'Etudiant':
+        displayStudentHomePage($conn);
+        break;
+    default:
+        displayDefaultHomePage($conn);
+        break;
+}
+
 ?>
