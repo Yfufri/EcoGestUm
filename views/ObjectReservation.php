cb36e1ba8b7225aea52d202bea15e0fa05ce0e93
allao <allaoui.mouad00@gmail.com>
2025-11-24 09:04:07 +0100

    Debut de ObjectReservation

---
+++ views/ObjectReservation.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/styleObjectReservation.css">
+
+<div class="container">
+    <h1>[Objet]</h1>
+    <div class="bloc-objet">
+        <div class="bloc-img">
+            <img src="assets/ObjectBrowser/imageDefautObjectBrowser.png" alt="Mug rouge" class="object-img">
+        </div>
+        <div class="object-info">
+            <div class="proprietaire"><p>Propriétaire : [Prénom] [Nom]</p></div>
+            <div class="description"><p>
+                Cet objet est proposé gratuitement dans le cadre du projet EcoGESTUM.<br>
+                Il est en très bon état et idéal pour une seconde vie.<br>
+                N'hésitez pas à consulter sa localisation sur la carte et à contacter le propriétaire pour plus d'informations ou pour convenir d'un rendez-vous.
+            </p>
+            </div>
+            <button class="btn-reserve">Réserver</button>
+        </div>
+    </div>
+</div>
