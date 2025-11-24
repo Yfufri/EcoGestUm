298bb4f5c0a93cc986e464387ede9555a92a4f79
Yfufri <theolannier50@gmail.com>
2025-11-24 15:40:34 +0100

    remis les bonnes statistiques sur la page d'accueil

--- controllers/Home.php
+++ controllers/Home.php
@@ -1,7 +1,15 @@
 <?php
-        include "views/banner.php";
-        include "views/DiscoverPolitique.php";
-        include "controllers/Statistics.php";
-        include "views/Events.php";
-        include "views/visite.php";
+
+include "models/gererEquipement.php";
+include "models/gererEvenement.php";
+
+$nbObjetRecycle = getNbObjectRecycled($conn);
+$nbEvenementPasse = getNbPastEvent($conn);
+$nbObjetDisponible = getNbObjectDisponible($conn);
+
+include "views/banner.php";
+include "views/DiscoverPolitique.php";
+include "views/Statistics.php";
+include "views/Events.php";
+include "views/visite.php";
 ?>
