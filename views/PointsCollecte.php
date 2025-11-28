65c9303cfc548af399033f7a9421064f3d0a9924
Yanis <Yanis.BOukhedra06013@gmail.com>
2025-11-28 11:31:37 +0100

    fix zoom "LeMans"/ "Laval" map

--- views/PointsCollecte.php
+++ views/PointsCollecte.php
@@ -65,11 +65,10 @@
 
  function showMans() {
     // Université du Mans
-    map.setView([48.01698, 0.1616], 16); // zoom plus proche
+    map.setView([48.01698, 0.1616], 16); 
 }
     
 function showLaval() {
     // Campus universitaire de Laval
-    map.setView([48.0866, -0.75796], 16); // ajuste le zoom (16–17) selon ce que tu préfères
-}
+    map.setView([48.0866, -0.75796], 16); 
 </script>
