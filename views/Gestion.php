9009d349c51b5542bb4c52dd2eb2bbdf4520eac3
Yfufri <theolannier50@gmail.com>
2025-11-28 15:58:01 +0100

    modif image gestion

--- views/Gestion.php
+++ views/Gestion.php
@@ -4,7 +4,7 @@
 <section class="gestion">
     <h2>Gestion de mes objets</h2>
     <div class="content">
-        <img src="assets/accueil/image4.png" alt="Gestion" class="gestion-img">
+        <img src="assets/accueil/image5.png" alt="Gestion" class="gestion-img">
         <div class="buttons">
             <a href="?action=mesObjets" class="btnMesObjets">Voir mes objets</a>
             <a href="?action=mesReservation" class="btnMesReservations">Voir mes reservations</a>
